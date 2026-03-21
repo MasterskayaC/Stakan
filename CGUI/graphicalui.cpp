cb90ac96f7ee641cac1818b7249cf48ee0165496
Aleksandr <aleks.ryzhov82@gmail.com>
2026-03-21 12:55:55 +0400

    переделал под использование QtDesigner
    переделал под использование QtDesigner созданы тестовые элементы управления.

--- CGUI/graphicalui.cpp
+++ CGUI/graphicalui.cpp
@@ -1,48 +1,16 @@
 #include"graphicalui.h"
-#include <QVBoxLayout>
-#include <QMessageBox>
-#include <QEventLoop>
 
-GraphicalUI::GraphicalUI() {
-    window = new QMainWindow();
-    textDisplay = new QTextEdit();
-    inputLine = new QLineEdit();
-    progressBar = new QProgressBar();
-    
-    // Создаем центральный виджет и layout
-    QWidget* central = new QWidget();
-    QVBoxLayout* layout = new QVBoxLayout(central);
-    layout->addWidget(textDisplay);
-    layout->addWidget(inputLine);
-    layout->addWidget(progressBar);
-    
-    window->setCentralWidget(central);
-    window->show();
+GraphicalUI::GraphicalUI(QWidget *parent):QMainWindow(parent){
+    ui.setupUi(this);   
+    connect(ui.pushButton1,&QPushButton::clicked,this,&GraphicalUI::PushButton1Clicked);
+    connect(ui.actiontest_sub,&QAction::triggered,this,&GraphicalUI::TestSubClicked);
 }
 
-void GraphicalUI::ShowMessage(const std::string& msg) {
-    textDisplay->append(QString::fromStdString(msg));
+void GraphicalUI::TestSubClicked(){
+    QString user_name = QInputDialog::getText(this,"","Input User name: ");
+    QString password = QInputDialog::getText(this,"","input password :");
 }
 
-std::string GraphicalUI::GetUserInput(const std::string& prompt) {
-    ShowMessage(prompt);
-    QEventLoop loop;
-    connect(inputLine, &QLineEdit::returnPressed, &loop, &QEventLoop::quit);
-    loop.exec();
-    QString result = inputLine->text();
-    inputLine->clear();
-    return result.toStdString();
-}
-
-void GraphicalUI::DisplayError(const std::string& error) {
-    QMessageBox::critical(window, "Ошибка", 
-                          QString::fromStdString(error));
-}
-
-void GraphicalUI::ShowProgress(int percent) {
-    progressBar->setValue(percent);
-}
-
-void GraphicalUI::Clear() {
-    textDisplay->clear();
+void GraphicalUI::PushButton1Clicked(){
+    QMessageBox::information(this, "Привет", "Кнопка нажата!");
 }
