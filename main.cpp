a3205c00ecbd2ba3d0cad9a2aec4649e6d59c46d
demureronin <medvedmorkov@gmail.com>
2026-03-21 12:22:24 +0300

    main file clean

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
-#include "src/database/PostgresConnector.h"
 
 int main() {
     std::cout << "Eto birjevoy stakan!" << std::endl;
-
-    PostgresConnector conn;
     return 0;
 }
