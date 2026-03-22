5cf69e3068b4ea82cb31f5a0e9fe31b7d9e2fd72
Aleksandr <aleks.ryzhov82@gmail.com>
2026-03-22 19:04:16 +0400

    Add files via upload

--- CGUI/graphicalui.h
+++ CGUI/graphicalui.h
@@ -8,15 +8,22 @@
 #include <QPushButton>
 #include <QVBoxLayout>
 #include"ui_mainwindow.h"
+
+class IOrderBookClient;
+
 class GraphicalUI:public QMainWindow{
     Q_OBJECT
      Ui_MainWindow ui;
-    
+     IOrderBookClient* client;
 public:
-    GraphicalUI(QWidget *parent = nullptr);
+    GraphicalUI()=default;
+    GraphicalUI(const IOrderBookClient* client, QWidget *parent = nullptr);
     static int Run(int argc, char* argv[]);
+    void notify();
 private:
+
     void TestSubClicked();
     void PushButton1Clicked();
 };
 
+void notify(GraphicalUI& gui);
