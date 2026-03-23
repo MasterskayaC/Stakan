5b5a4e8df283043ad683ad3ee5acca348f417b95
wannabeanything <stas05728@gmail.com>
2026-03-23 20:46:04 +0300

    all_advices_followed

--- src/bid_ask_interface.h
+++ src/bid_ask_interface.h
@@ -11,42 +11,46 @@ namespace common
     using ID = uint64_t;
     using Price = int64_t;
 
+
+    enum class Side {Bid, Ask};
+
+    template<class Archive>
+    void serialize(Archive& ar, Side& side, [[maybe_unused]] const unsigned int version){
+        ar << side;
+    }
+
+
+    std::string SideToString (Side side);
+
     struct Order {
         Order() = default;
-        Order(ID number, Price p, int qty, bool active);
+        Order(ID number, Side s, Price p, int qty);
         ID id = 0;
+        Side side = Side::Ask;
         Price price = 0;
         int quantity = 0;
-        bool active = true;
-    };
 
-    struct TopLevel
-    {
-        Price price;
-        int quantity;
-        TopLevel(Price p, int qty);
-        template <class Archive>
-        void serialize(Archive &ar, [[maybe_unused]] const unsigned int version)
-        {
-            ar & price;
-            ar & quantity;
+        template<class Archive>
+        void serialize(Archive& ar, [[maybe_unused]] const unsigned int version){
+            ar << id;
+            ar << side;
+            ar << price;
+            ar << quantity;
         }
     };
 
-    struct Snapshot
-    {
-        Snapshot(std::vector<TopLevel> bids, std::vector<TopLevel> asks);
-        std::vector<TopLevel> topBids;
-        std::vector<TopLevel> topAsks;
-
-        template <class Archive>
-        void serialize(Archive &ar, [[maybe_unused]] const unsigned int version)
-        {
-            ar & topBids;
-            ar & topAsks;
+    struct Snapshot {
+        Snapshot() = default;
+        Snapshot(std::vector<const Order*> bids, std::vector<const Order*> asks);
+        std::vector<const Order*> topBids;
+        std::vector<const Order*> topAsks;
+        template<class Archive>
+        void serialize(Archive& ar, [[maybe_unused]] const unsigned int version){
+            ar << topBids;
+            ar << topAsks;
         }
     };
-
+    // @brief Функции для се/десериализации snapshot
     inline std::vector<uint8_t> SerializeSnapshot(const Snapshot &snap);
     inline Snapshot DeserializeSnapshot(const std::vector<uint8_t>& serialized_snapshot);
 }
