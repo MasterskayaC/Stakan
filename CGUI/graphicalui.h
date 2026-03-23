c0efe4986319a7b290d91d0ba55a4b0c2ef11a57
Aleksandr <aleks.ryzhov82@gmail.com>
2026-03-23 17:23:57 +0400

    Добавил методы для запуска внутри колбэков

--- CGUI/graphicalui.h
+++ CGUI/graphicalui.h
@@ -10,6 +10,9 @@
 #include"ui_mainwindow.h"
 
 class IOrderBookClient;
+struct Snapshot;
+struct TopLevel;
+struct ClientError;
 
 class GraphicalUI:public QMainWindow{
     Q_OBJECT
@@ -19,12 +22,15 @@ public:
     GraphicalUI()=default;
     GraphicalUI(const IOrderBookClient* client, QWidget *parent = nullptr);
     static int Run(int argc, char* argv[]);
-    void notify();
-    void LoadConfiguration();
+    
 private:
-
+    void OnConnected();
+    void OnDisconnected();
+    void OnStateChanged();
+    void OnSnapshot(const Snapshot&);
+    void OnTopOfBook(const TopLevel&);
+    void OnError(ClientError, std::string_view);
     void TestSubClicked();
     void PushButton1Clicked();
 };
 
-void notify(GraphicalUI& gui);
