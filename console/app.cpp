b87bb99371fdae87670450f3c653170d97a857c3
GoshanNaSmene <igor.promzelev@gmail.com>
2026-03-26 18:51:14 +0300

    CC-002 (#24)
    Console Client: Base Architecture
    What’s Done
    Defined the base structure of the console client and introduced the core components.
    
    Application - Responsible for application lifecycle and initialization.
    Application()- Establishes connection to the server
    Run() - Starts the main application loop
    Menu
    Encapsulates user input handling and command routing.
    
    AddCommand() - adds a command
    AddArgsParser() - adds arguments and a parser for them
    Run() - Starts the command processing loop
    CommandHandler
    Implements command handlers and registers them in the menu.
    
    CommandHandler(Menu&) - Accepts a reference to Menu and registers handlers
    
    Supported commands:
    
    Buy()
    Sell()
    Book()
    Exit()
    Help()
    ArgsParsers
    Implements the argument parser command and registers them in the menu.
    
    ArgsParsers(Menu&) - Accepts a reference to Menu and registers parsers
    
    Supported commands:
    
    ParseBuyArgs()
    ParseSellArgs()
    ParseBookArgs()
    Build
    Added CMakeLists.txt for the client and connected it to the root project
    
    cmake -B build
    cmake --build build

---
+++ console/app.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "app.hpp"
+#include "menu.hpp"
+#include "menu_action.hpp"
+
+namespace app {
+
+Application::Application() {}
+
+void Application::Run() {}
+
+} // namespace app
