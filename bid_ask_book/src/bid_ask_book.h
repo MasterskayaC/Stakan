962b79ceecbadc16b319cf74a3f532ab7d0d89ea
Anton <scart94@gmail.com>
2026-03-26 18:58:19 +0300

    S-009-bid/ask_book: Add initial implementation (#25)
    Реализация bid/ask_book
    1. Основные методы OrderBook
    NewBid — вставка ордера в нужный контейнер (bid/ask)
    NewAsk — вставка ордера в нужный контейнер (bid/ask)
    CancelBid — удаление ордера
    CancelAsk — удаление ордера
    ReplaceBid — замена значений
    ReplaceAsk — замена значений
    GetTopSnapshot — возвращает топ N ордеров по каждому направлению, указатели на реальные объекты.
    BestBid/BestAsk — быстрый доступ к лучшей цене через ordered index.
    данные храним в boost::multi_index
    
    2. Логирование
    Введён простой Logger с уровнями Info, Warning, Error.
    Логируются события: добавление, ошибка формата, отмена/замена ордеров.
    3. Юнит-тесты
    Написаны тесты через Catch2
    4. CMake
    Настроен простой CMakeLists.txt для сборки bid_ask_book.cpp и tests.cpp
    Подключены Catch2 (3.4.0) и boost (1.90) как dependency

---
+++ bid_ask_book/src/bid_ask_book.h
@@ -0,0 +1,95 @@
+#pragma once
+
+#include <chrono>
+#include <shared_mutex>
+
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+#include <boost/multi_index/hashed_index.hpp>
+#include <boost/multi_index/member.hpp>
+#include "bid_ask_interface.h"
+
+namespace server {
+    enum class LogLevel {
+        Info,
+        Warning,
+        Error
+    };
+    // первоначальная имплементация базового логгера, ToDo - перенести его в более подходящее место
+    class Logger {
+    public:
+        static void Log(LogLevel level, const std::string& msg);
+
+    private:
+        static std::string Now();
+        static const char* ToString(LogLevel level);
+    };
+
+    class OrderBook {
+    public:
+        //отдельный метод для каждого контейнера
+        void NewBid(common::Order order);
+        void NewAsk(common::Order order);
+        void CancelBid(common::ID order_id);
+        void CancelAsk(common::ID order_id);
+        void ReplaceBid(common::Order old_order, common::Order new_order);
+        void ReplaceAsk(common::Order old_order, common::Order new_order);
+
+        [[nodiscard]] common::Snapshot GetTopSnapshot() const;
+
+        [[nodiscard]] common::Order BestBid() const;
+        [[nodiscard]] common::Order BestAsk() const;
+
+    private:
+        struct BidComparator {
+            bool operator()(const common::Order& a, const common::Order& b) const {
+                if (a.price != b.price) return a.price > b.price;
+                if (a.quantity != b.quantity) return a.quantity > b.quantity;
+                return a.id < b.id;
+            }
+        };
+        struct AskComparator {
+            bool operator()(const common::Order& a, const common::Order& b) const {
+                if (a.price != b.price) return a.price < b.price;
+                if (a.quantity != b.quantity) return a.quantity > b.quantity;
+                return a.id < b.id;
+            }
+        };
+
+        // то же что set (сортировка по цене) + map <Id, it>, только атомарный + надежнее
+        using BidContainer = boost::multi_index::multi_index_container<
+            common::Order,
+            boost::multi_index::indexed_by<
+                // индекс по цене
+                boost::multi_index::ordered_non_unique<
+                    boost::multi_index::identity<common::Order>,
+                    BidComparator
+                >,
+                // индекс по id
+                boost::multi_index::hashed_unique<
+                    boost::multi_index::member<common::Order, common::ID, &common::Order::id>
+                >
+            >
+        >;
+
+        using AskContainer = boost::multi_index::multi_index_container<
+            common::Order,
+            boost::multi_index::indexed_by<
+                boost::multi_index::ordered_non_unique<
+                    boost::multi_index::identity<common::Order>,
+                    AskComparator
+                >,
+                boost::multi_index::hashed_unique<
+                    boost::multi_index::member<common::Order, common::ID, &common::Order::id>
+                >
+            >
+        >;
+
+        BidContainer bids_;
+        AskContainer asks_;
+
+        // мьютексы для потокобезопасности
+        mutable std::shared_mutex bids_mutex_;
+        mutable std::shared_mutex asks_mutex_;
+    };
+}
--- bid_ask_snaphot_interface/bid_ask_interface.h
+++ bid_ask_snaphot_interface/bid_ask_interface.h
@@ -8,11 +8,14 @@
 
 namespace common
 {
+    constexpr uint8_t topN = 20;
+
     using ID = uint64_t;
     using Price = uint64_t;
     struct Order {
         Order() = default;
-        Order(ID number, Price p, int qty);
+        Order(ID number, Price p, int qty)
+            : id(number), price(p), quantity(qty) {}
         ID id = 0;
         Price price = 0;
         int quantity = 0;
@@ -28,9 +31,10 @@ namespace common
     struct Snapshot
     {
         Snapshot() = default;
-        Snapshot(std::vector<Order> bids, std::vector<Order> asks);
-        std::vector<Order> topBids;
-        std::vector<Order> topAsks;
+        Snapshot(std::array<common::Order, topN> bids, std::array<common::Order, topN> asks);
+        std::array<common::Order, topN> topBids;
+        std::array<common::Order, topN>  topAsks;
+
         template<class Archive>
         void serialize(Archive& ar, [[maybe_unused]] const unsigned int version){
             ar << topBids;
