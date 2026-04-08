9b9c0723e131f08fef9571945de151685ba68872
wannabeanything <stas05728@gmail.com>
2026-04-08 14:26:52 +0300

    removed client_list.*

--- src/client_list/client_list.cpp
+++ src/client_list/client_list.cpp
@@ -1,218 +1,102 @@
 #include "client_list.h"
 
-#include <mutex>
-#include <stdexcept>
 #include <unordered_map>
-
-#include "command_queue.h"
 
 class ClientList : public IClientList {
 private:
     struct ClientContext {
         SessionPtr session;
-        std::vector<std::shared_ptr<common::Order>> bids;
-        std::vector<std::shared_ptr<common::Order>> asks;
+        std::vector<std::shared_ptr<Order>> bids;
+        std::vector<std::shared_ptr<Order>> asks;
         bool subscribed = false;
     };
     std::unordered_map<ClientId, ClientContext> clients_;
-    std::unordered_map<const Session*, ClientId> session_to_client_;
     mutable std::mutex mutex_;
-
 public:
+
     void add_session(ClientId id, SessionPtr session) override {
-        std::lock_guard<std::mutex> lock(mutex_);
-        const auto it = clients_.find(id);
-        if (it != clients_.end() && it->second.session) {
-            session_to_client_.erase(it->second.session.get());
-        }
-        clients_[id].session = std::move(session);
-        if (clients_[id].session) {
-            session_to_client_[clients_[id].session.get()] = id;
-        }
+        return;
     }
 
     void remove_session(ClientId id) override {
-        std::lock_guard<std::mutex> lock(mutex_);
-        const auto it = clients_.find(id);
-        if (it != clients_.end() && it->second.session) {
-            session_to_client_.erase(it->second.session.get());
-        }
-        clients_.erase(id);
+        return;
     }
 
     SessionPtr get_session(ClientId id) const override {
-        std::lock_guard<std::mutex> lock(mutex_);
-        const auto it = clients_.find(id);
-        if (it == clients_.end()) {
-            return {};
-        }
-        return it->second.session;
+        return SessionPtr{};
     }
 
     size_t size() const override {
-        std::lock_guard<std::mutex> lock(mutex_);
-        return clients_.size();
+        return 0;
     }
 
     std::vector<SessionPtr> get_all_sessions() const override {
         std::vector<SessionPtr> tmp;
-        std::lock_guard<std::mutex> lock(mutex_);
-        tmp.reserve(clients_.size());
-        for (const auto& [id, ctx] : clients_) {
-            (void)id;
-            if (ctx.session) {
-                tmp.push_back(ctx.session);
-            }
-        }
         return tmp;
     }
 
     bool has_client(ClientId id) const override {
-        std::lock_guard<std::mutex> lock(mutex_);
-        return clients_.find(id) != clients_.end();
+        return false;
     }
 
-    void add_bid(ClientId id, std::shared_ptr<common::Order> bid) override {
-        std::lock_guard<std::mutex> lock(mutex_);
-        const auto it = clients_.find(id);
-        if (it == clients_.end()) {
-            return;
-        }
-        it->second.bids.push_back(std::move(bid));
+    void add_bid(ClientId id, std::shared_ptr<Order> bid) override {
+        return;
     }
 
-    void add_ask(ClientId id, std::shared_ptr<common::Order> ask) override {
-        std::lock_guard<std::mutex> lock(mutex_);
-        const auto it = clients_.find(id);
-        if (it == clients_.end()) {
-            return;
-        }
-        it->second.asks.push_back(std::move(ask));
+    void add_ask(ClientId id, std::shared_ptr<Order> ask) override {
+        return;
     }
 
-    void remove_bid(ClientId id) override {
-        std::lock_guard<std::mutex> lock(mutex_);
-        auto it = clients_.find(id);
-        if (it != clients_.end() && !it->second.bids.empty()) {
-            it->second.bids.pop_back();
-        }
+    void remove_bid(ClientId id, OrderId bid_id) override {
+        return;
     }
 
-    void remove_ask(ClientId id) override {
-        std::lock_guard<std::mutex> lock(mutex_);
-        auto it = clients_.find(id);
-        if (it != clients_.end() && !it->second.asks.empty()) {
-            it->second.asks.pop_back();
-        }
+    void remove_ask(ClientId id, OrderId ask_id) override {
+        return;
     }
 
-    std::vector<std::shared_ptr<common::Order>> get_bids(ClientId id) const override {
-        std::vector<std::shared_ptr<common::Order>> tmp;
-        std::lock_guard<std::mutex> lock(mutex_);
-        const auto it = clients_.find(id);
-        if (it != clients_.end()) {
-            tmp = it->second.bids;
-        }
+    std::vector<std::shared_ptr<Order>> get_bids(ClientId id) const override {
+        std::vector<std::shared_ptr<Order>> tmp;
         return tmp;
     }
 
-    const std::vector<std::shared_ptr<common::Order>>& get_bids_ref(ClientId id) const override {
-        std::lock_guard<std::mutex> lock(mutex_);
-        const auto it = clients_.find(id);
-        if (it != clients_.end()) {
-            return it->second.bids;
-        }
+    const std::vector<std::shared_ptr<Order>>& get_bids_ref(ClientId id) const override {
         throw std::out_of_range("Client not found");
     }
 
-    std::vector<std::shared_ptr<common::Order>> get_asks(ClientId id) const override {
-        std::vector<std::shared_ptr<common::Order>> tmp;
-        std::lock_guard<std::mutex> lock(mutex_);
-        const auto it = clients_.find(id);
-        if (it != clients_.end()) {
-            tmp = it->second.asks;
-        }
+    std::vector<std::shared_ptr<Order>> get_asks(ClientId id) const override {
+        std::vector<std::shared_ptr<Order>> tmp;
         return tmp;
     }
 
-    const std::vector<std::shared_ptr<common::Order>>& get_asks_ref(ClientId id) const override {
-        std::lock_guard<std::mutex> lock(mutex_);
-        const auto it = clients_.find(id);
-        if (it != clients_.end()) {
-            return it->second.asks;
-        }
+    const std::vector<std::shared_ptr<Order>>& get_asks_ref(ClientId id) const override {
         throw std::out_of_range("Client not found");
     }
 
     bool is_subscribed(ClientId id) const override {
-        std::lock_guard<std::mutex> lock(mutex_);
-        const auto it = clients_.find(id);
-        return it != clients_.end() && it->second.subscribed;
+        return false;
     }
 
     void subscribe(ClientId id) override {
-        std::lock_guard<std::mutex> lock(mutex_);
-        const auto it = clients_.find(id);
-        if (it == clients_.end()) {
-            return;
-        }
-        it->second.subscribed = true;
+        return;
     }
 
     void unsubscribe(ClientId id) override {
-        std::lock_guard<std::mutex> lock(mutex_);
-        auto it = clients_.find(id);
-        if (it != clients_.end()) {
-            it->second.subscribed = false;
-        }
+        return;
     }
-
 
     std::vector<ClientId> get_subscribed_clients() const override {
         std::vector<ClientId> tmp;
-        std::lock_guard<std::mutex> lock(mutex_);
-        for (const auto& [id, ctx] : clients_) {
-            if (ctx.subscribed) {
-                tmp.push_back(id);
-            }
-        }
         return tmp;
     }
 
     std::vector<SessionPtr> get_subscribed_sessions() const override {
-        std::lock_guard lck(mutex_);
-        std::vector<SessionPtr> res;
-        res.reserve(clients_.size());
-        for (auto [client_id, client_context] : clients_) {
-            if (client_context.subscribed) {
-                res.push_back(client_context.session);
-            }
-        }
-        return res;
+        std::vector<SessionPtr> tmp;
+        return tmp;
     }
 
     void broadcast_to_subscribed(const std::vector<char>& message) override {
-        std::vector<SessionPtr> sub_sessions = get_subscribed_sessions();
-        for (SessionPtr s : sub_sessions) {
-            s->SendMsg(message);
-        }
+        return;
     }
 
-    void broadcast_to_certain(ClientId id, const std::vector<char>& message) override {
-        SessionPtr s = get_session(id);
-        s->SendMsg(message);
-    }
-
-    std::optional<ClientId> find_client_id_by_session(const Session* session) const override {
-        std::lock_guard<std::mutex> lock(mutex_);
-        const auto it = session_to_client_.find(session);
-        if (it == session_to_client_.end()) {
-            return std::nullopt;
-        }
-        return it->second;
-    }
 };
-
-std::unique_ptr<IClientList> makeClientList() {
-    return std::make_unique<ClientList>();
-}
