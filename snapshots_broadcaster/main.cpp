d5db02bab945bbf8d56e177db07c6f6d87e62b04
Rinalii <arinamak02@mail.ru>
2026-04-11 20:01:56 +0300

    S-004-2-send-snapshot-to-broadcaster (#44)
    DOMManager is the central component that manages the entire process of generating and distributing DOM snapshots. It accepts commands (adding/deleting requests, starting/stopping periodic distribution, sending a snapshot to a specific client) through operator overload (), which puts them in the io_context queue for asynchronous execution. Depending on the settings, the snapshots can be either test ones generated by TmpSnapshotCreator, or real ones from an OrderBook.

---
+++ snapshots_broadcaster/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <memory>
+
+#include "../bid_ask_book/src/bid_ask_book.h"
+#include "dom_manager.h"
+
+int main() {
+    std::cout << "Eto birjevoy stakan!" << std::endl;
+
+    // Create io_context and DOMManager
+    boost::asio::io_context io_context;
+    int num_of_snapshots = 5;
+    bool is_random = false;
+    std::unique_ptr<server::OrderBook> order_book = std::make_unique<server::OrderBook>();
+    std::unique_ptr<IClientList> client_list = makeClientList();
+    server::DOMManager dom(io_context, *client_list, std::move(order_book));
+
+    // Run io_context in a separate thread
+    std::thread io_thread([&io_context]() {
+        io_context.run();
+    });
+
+    // Send command to start broadcasting
+    int broadcast_interval_ms = 3000;
+    bool use_test_broadcast = true;
+    server::StartBroadcastCmd br{broadcast_interval_ms, use_test_broadcast};
+    dom(br);  // operator() passes the command to io_context
+
+    // Simulate application work (10 seconds)
+    std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+
+    // Stop broadcasting
+    server::StopBroadcastCmd stop;
+    dom(stop);
+
+    // Stop io_context and wait for thread to finish
+    io_context.stop();
+    if (io_thread.joinable()) {
+        io_thread.join();
+    }
+
+    std::cout << "Eto birjevoy stakan end!" << std::endl;
+    return 0;
+}
