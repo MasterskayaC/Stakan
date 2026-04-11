8c13268552bf3a3384cbf39620dc7e7db41925a2
VelesZero <nikkita.04@mail.ru>
2026-04-11 17:36:37 +0300

    upgrade code for new cmake

--- console/app.h
+++ console/app.h
@@ -1,11 +1,11 @@
 #pragma once
 
 #include <cstdint>
-#include <string>
 #include <memory>
+#include <string>
 
+#include "client_lib_interface.hpp"
 #include "snapshot_client.h"
-#include "order_book_client.hpp"
 
 namespace menu {
 class Menu;
@@ -16,22 +16,22 @@ namespace app {
 
 class Application final {
 public:
-    explicit Application();
-    ~Application();
+  explicit Application();
+  ~Application();
 
-    void Run();
-    bool connect(const std::string& host, uint16_t port);
-    bool fetch_snapshot();
-
-private:
-    void on_snapshot_received(const client_lib::Snapshot& snapshot);
-    void on_error(const std::string& error);
+  void Run();
+  bool connect(const std::string &host, uint16_t port);
+  bool fetch_snapshot();
 
 private:
-    std::shared_ptr<client_lib::IOrderBookClient> order_book_client_;
-    std::shared_ptr<console::SnapshotConsoleClient> snapshot_client_;
-    std::unique_ptr<menu::Menu> menu_;
-    std::unique_ptr<menu::CommandHandlers> command_handlers_;
+  void on_snapshot_received(const client_lib::Snapshot &snapshot);
+  void on_error(const std::string &error);
+
+private:
+  std::shared_ptr<client_lib::IOrderBookClient> order_book_client_;
+  std::shared_ptr<console::SnapshotConsoleClient> snapshot_client_;
+  std::unique_ptr<menu::Menu> menu_;
+  std::unique_ptr<menu::CommandHandlers> command_handlers_;
 };
 
 } // namespace app
--- console/snapshot_client.h
+++ console/snapshot_client.h
@@ -1,42 +1,42 @@
 #pragma once
 
 #include <cstdint>
-#include <string>
-#include <memory>
 #include <functional>
+#include <memory>
+#include <string>
 
 #include "client_lib_interface.hpp"
 
 namespace console {
 
-using SnapshotCallback = std::function<void(const client_lib::Snapshot&)>;
-using ErrorCallback = std::function<void(const std::string&)>;
+using SnapshotCallback = std::function<void(const client_lib::Snapshot &)>;
+using ErrorCallback = std::function<void(const std::string &)>;
 
 class SnapshotConsoleClient {
 public:
-    explicit SnapshotConsoleClient(std::shared_ptr<client_lib::IOrderBookClient> client);
-    ~SnapshotConsoleClient();
+  explicit SnapshotConsoleClient(
+      std::shared_ptr<client_lib::IOrderBookClient> client);
+  ~SnapshotConsoleClient();
 
-    void set_snapshot_callback(SnapshotCallback callback);
-    void set_error_callback(ErrorCallback callback);
+  void set_snapshot_callback(SnapshotCallback callback);
+  void set_error_callback(ErrorCallback callback);
 
-    void connect_to_server(const std::string& host, uint16_t port);
-    void disconnect_from_server();
-    void fetch_snapshot();
-    bool is_connected() const;
-
-private:
-    void on_connected();
-    void on_disconnected();
-    void on_snapshot(const client_lib::Snapshot& snapshot);
-    void on_error(client_lib::ClientError error, std::string_view message);
+  void connect_to_server(const std::string &host, uint16_t port);
+  void disconnect_from_server();
+  void fetch_snapshot();
+  bool is_connected() const;
 
 private:
-    std::shared_ptr<client_lib::IOrderBookClient> client_;
-    client_lib::ClientConfig config_;
-    SnapshotCallback snapshot_callback_;
-    ErrorCallback error_callback_;
-    bool connected_ = false;
+  void on_connected();
+  void on_disconnected();
+  void on_snapshot(const client_lib::Snapshot &snapshot);
+  void on_error(client_lib::ConnectionState state, std::string_view message);
+
+private:
+  std::shared_ptr<client_lib::IOrderBookClient> client_;
+  SnapshotCallback snapshot_callback_;
+  ErrorCallback error_callback_;
+  bool connected_ = false;
 };
 
 } // namespace console
