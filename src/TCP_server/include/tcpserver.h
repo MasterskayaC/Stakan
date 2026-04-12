06dec4046b9b4c35598d5cd75654e28ae65d56ff
Nester0v474 <misha2004nsk@gmail.com>
2026-04-13 00:25:57 +0700

    Update tcpserver.h

--- src/TCP_server/include/tcpserver.h
+++ src/TCP_server/include/tcpserver.h
@@ -15,12 +15,34 @@ class TCPServer :public std::enable_shared_from_this<TCPServer> {
 public:
     TCPServer(boost::asio::io_context& io_context, unsigned short port);
     ~TCPServer();
+    /**
+     *  @brief Init socket and  do_accept
+    */
     void StartServer();
+
+    /**
+     *  @brief Close acceptor
+    */
     void StopServer();
+
+    /**
+     *  @brief Aend update msg to all client
+     *  @param Update message
+    */
     void SendUpdateMessage(const std::string& message);
 
 private:
+
+    /**
+     *  @brief Init async waiting new connection
+    */
     void DoAccept();
+
+    /**
+     *  @brief Create socket ptr, set session to sessions container
+     *  @param Socket
+     *  @param Error object
+    */
     std::shared_ptr<Session> OnAccept(std::shared_ptr<tcp::socket> socket,
                    const boost::system::error_code& error);
     void HandleMessage(const std::vector<std::uint8_t>& frame, const std::shared_ptr<Session>& session);
