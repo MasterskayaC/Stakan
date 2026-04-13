53ab00e4d0a02132042381c3fe09627af5d05030
Nester0v474 <misha2004nsk@gmail.com>
2026-04-13 17:51:27 +0700

    Update session.h

--- src/TCP_server/include/session.h
+++ src/TCP_server/include/session.h
@@ -15,18 +15,49 @@ public:
     using OnDisconnectCallback = std::function<void(const std::shared_ptr<Session>&)>;
 
     Session(std::shared_ptr<boost::asio::ip::tcp::socket> socket);
+ /**
+     *  @brief Start async reading
+        Comment view    
+        */
     void Start();
+/**
+     *  @brief Close socket and stop all operations
+    */
     void Stop();
+ /**
+     *  @brief Push msg to messages queue and start writing to stream
+     *  @param Message
+    */
     /// Enqueues bytes; actual socket I/O is performed by @c boost::asio::async_write in Write().
     void SendMsg(const std::vector<char>& message);
     bool IsOpen() const;
     void SetCallbacks(OnDataCallback on_data, OnDisconnectCallback on_disconnect);
 
 private:
+/**
+     *  @brief Async reading from socket
+    */
     void Read();
+ /**
+     *  @brief Check error and read "bytes_transferred" bytes
+     *  @param Error object
+     *  @param Numbers reading bytes
+    */
     void ProcessRead(const boost::system::error_code& error, std::size_t bytes_transferred);
+/**
+     *  @brief Async writing to stream
+    */
     void Write();
+/**
+     *  @brief Check error and write "bytes_transferred" bytes
+     *  @param Error object
+     *  @param Numbers bytes to writing
+    */
     void ProcessWrite(const boost::system::error_code& error, std::size_t bytes_transferred);
+/**
+     *  @brief Serialize and send msg
+     *  @param Order or update struct
+    */
 
     std::shared_ptr<boost::asio::ip::tcp::socket> socket_;
     boost::asio::streambuf read_buffer_;
