96c0a6a5b878e406ec87060b07e47dc32bc0e20b
Aleksandr <aleks.ryzhov82@gmail.com>
2026-04-16 21:03:12 +0400

    поправил тип функции

--- src/snapshots_broadcaster/snapshot_source.h
+++ src/snapshots_broadcaster/snapshot_source.h
@@ -25,8 +25,8 @@ public:
     virtual ~ISnapshotSource() = default;
 
     virtual std::optional<common::Snapshot> get_snapshot() = 0;
-    virtual double GetNewBid() = 0;
-    virtual double GetNewAsk() = 0;
+    virtual int64_t GetNewBid() = 0;
+    virtual int64_t GetNewAsk() = 0;
 };
 
 /**
