40f80b2cefb3fbb1d960979dc4ed4b664aaa58ed
Aleksandr <aleks.ryzhov82@gmail.com>
2026-04-21 19:14:47 +0400

    adding GetNewBid and GetNewAsk (#92)
    * удалил директорию для переноса
    
    * Добавил директорию которую удалил из корня
    
    * добавил правильный путь к snapshot_broadcaster
    
    * добавил то что точно необходимо и убрал то что точно ненужно
    
    * добавил функции
    
    * убрал коментарии лишние
    
    * убрал коментарии к предварительному объявлению OrderBook
    
    * Add files via upload
    
    * убрал коментарий
    
    * Update order_book_snapshot_source.cpp
    
    * Update order_book_snapshot_source.h
    
    * Update snapshot_source.cpp
    
    * Update snapshot_source.h
    
    * чутка поменял структуру
    
    * убрал коментарий
    
    * поменял тип ask и bid с вещественного на целый
    
    * поправил формат
    
    * поправил формат
    
    * поправил формат и тип функций
    
    * поправил тип функций
    
    * поправил формат
    
    * поправил формат
    
    * поправил формат
    
    * поправил тип функции
    
    * поправил методв order_book_snapshot
    
    * поменял тип с int64_t на common::Order

--- src/snapshots_broadcaster/order_book_snapshot_source.h
+++ src/snapshots_broadcaster/order_book_snapshot_source.h
@@ -1,8 +1,10 @@
 #pragma once
+#include <random>
 #include "snapshot_source.h"
 
 #include "../bid_ask_book/src/bid_ask_book.h"
 #include "../bid_ask_snaphot_interface/bid_ask_interface.h"
+
 
 
 class OrderBookSnapshotSource : public ISnapshotSource {
@@ -10,11 +12,14 @@ class OrderBookSnapshotSource : public ISnapshotSource {
     //в реализацию передается сырой указатель, однако лучше поменять на умный,
     //в зависимости от принципов владения OrderBookом
     OrderBookSnapshotSource(server::OrderBook* order_book);
+    common::Order GetNewBid() override;
+    common::Order GetNewAsk() override;
 
     ~OrderBookSnapshotSource() override = default;
 
     std::optional<common::Snapshot> get_snapshot() override;
 private:
+    RandomGenerator rg_;
     server::OrderBook *order_book_;
 };
 
